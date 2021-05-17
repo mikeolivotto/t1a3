45d644c0a01a89dc9db86e441db8069a4bd9d8dd
mikeolivotto <michaelolivotto@gmail.com>
2021-05-17 16:34:15 +1000

    add user stories, pseudocode and test cases

--- src/index.rb
+++ src/index.rb
@@ -1,6 +1,21 @@
 # user story
 
+# user must be able to view the instructions
+# user should be able to select an answers
+# user should be given an overall score
+# user should be able to view the correct answers for questions they got wrong
 
 
 
 # pseudocode
+# 1. display a welcome message with instructions
+# 2. display multiple choice questions
+#     1. If user gets answer correct, increase score by 1
+#     2. If user gets answer wrong, add question to array of wrong answers
+# 3. Display total score
+# 4. Display correct answers
+
+# IDEAS
+# True / false answers
+# multiple choice answers
+# free text answers
--- src/spec/trivia_app_spec.rb
+++ src/spec/trivia_app_spec.rb
@@ -1,1 +1,6 @@
 # Test cases
+1. Test is the [something? user?] instance is created
+2. should display a welcome message and instructions
+3. should add answers to an array
+4. should count the number of correct answers
+5. should display correct answers to the questions the user got wrong
