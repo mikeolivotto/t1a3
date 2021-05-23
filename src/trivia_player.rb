9891da5cbab0b2b70366582fd728e51dc476eed4
mikeolivotto <michaelolivotto@gmail.com>
2021-05-23 13:33:21 +1000

    Add more comments

--- src/trivia_player.rb
+++ src/trivia_player.rb
@@ -1,25 +1,26 @@
 class TriviaPlayer
 
     attr_reader :name, :score, :player_answer
+    # initialise the game/player with the player's name, a score starting at zero, and an empty array for their answers
     def initialize(name)        
         @name = name
         @score = 0
         @player_answer = []
-        # access and parse the JSON file
+        # access and parse the JSON question file
         @@question_file = File.read('./questions.json')
         @@json = JSON.parse(@@question_file)
     end
 
+    # create a welcome message
     def welcome_msg
         puts "Welcome, #{@name}".colorize(:blue)
         puts ""
         puts "You will be given a series of questions. Please select the answer you believe to be correct."
     end
 
-
-
+    # logic for displaying questions and getting the user's answers
     def play_game
-        # Counter to dynamically display which question number is being displayed (useful if questions are delivered randomly)
+        # Counter to be used to dynamically display question number (useful if questions are delivered randomly)
         @question_counter = 1
         # begin the loop through the available questions
         @@json.each do |question|
@@ -52,12 +53,12 @@ class TriviaPlayer
     end
 
     def corrections
+        # display the correct answers to questions user got wrong (UNLESS they answeres all questions correctly)
         unless @score == @@json.length
             puts ""
             puts "These are the correct answers to the questions you got wrong"
             # loop through player_answer array, pull out the user's incorrect answers and display correct answers
             index_of_answer = 0
-    
             player_answer.each do |answer|
                 json_index = @@json[index_of_answer]
                 if answer != json_index["correct_answer"]
